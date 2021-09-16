94f4e2871b890a0c6779b103a15ebcf86ec46e1f
Leonardo <leonardolivelopes2@gmail.com>
2021-09-16 19:16:26 -0300

    get word and display typing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,83 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import wordList from './resources/words.json'
+
+
+const MAX_TYPED_KEYS = 30
+
+// pegar palaras aleatoriamente
+const getWord = () => {
+    const index = Math.floor(Math.random() * wordList.length)
+    const word = wordList[index]
+    return word.toLocaleLowerCase()
+    
+}
+
+const isValidKey = (key, word) => {
+    if(!word) return false;
+    const result = word.split('').includes(key);
+    return result
+}
+
+const Word = ({word, validKeys}) => {
+
+    if(!word) return null
+
+    const joinedKeys = validKeys.join('')
+    const matched = word.slice(0, joinedKeys.length)
+    const remainder = word.slice(joinedKeys.length)
+
+    console.log("joinedKeys.length", joinedKeys.length)
+    console.log(joinedKeys)
+    console.log(validKeys)
+
+    return (
+    <>
+        <span className="matched">{matched}</span>
+        <span className="remainder">{remainder}</span>
+    </>)
+}
 
 const App = () => {
-    return (<div className="container">
+
+    const [typedKeys, setTypedKeys] = useState([])
+    const [validKeys, setValidKeys] = useState([])
+    const [word, setWord] = useState('')
+
+    useEffect(() => {
+        setWord(getWord())
+    }, [])
+
+    const handleKeyDown = (e) => {
+        e.preventDefault();
+
+        // const key = e.key || mesmo coisa abaixo
+        const { key } = e
+
+        // pegar o valor antigo das chaves
+        setTypedKeys( prev => 
+            [...prev, key].slice(MAX_TYPED_KEYS * -1))
+
+        if(isValidKey(key, word)) {
+            setValidKeys((prev) => {
+                const isValidLength = prev.length <= word.length
+                const isNextChar = isValidLength && word[prev.length] === key
+
+                return isNextChar ? [...prev, key] : prev
+            })
+        }
+    }
+    
+    return (
+    <div className="container" tabIndex="0" onKeyDown={handleKeyDown}>
 
         <div className="valid-keys">
-
-            <span className="matched">emer</span>
-            <span className="remainder">son</span>
-
+            <Word word={word} validKeys={validKeys}/>
         </div>
 
         <div className="typed-keys">
             {/* teclas digitadas */}
-            asdfemerasdf
+            {typedKeys ? typedKeys.join(' ') : null}
         </div>
         <div className="completed-words">
             
@@ -24,7 +89,8 @@ const App = () => {
             
         </div>
 
-    </div>)
+    </div>
+    )
 }
 
 export default App;
